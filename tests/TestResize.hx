0ba92f8ba3143440967199407659afa0c5e811c8
Ben Merckx <ben@codeurs.be>
2022-08-23 10:30:56 +0200

    Divide tests

--- tests/TestResize.hx
+++ tests/TestResize.hx
@@ -5,7 +5,7 @@ import helder.Unit.suite;
 import helder.Unit.assert;
 import image.Image;
 
-final TestInfo = suite(test -> {
+final TestResize = suite(test -> {
   function resize(engine: Engine, extension: String) {
 		return Image.resize(
 			'tests/assets/sample.$extension', 
@@ -15,41 +15,33 @@ final TestInfo = suite(test -> {
 				engine: engine
 			}).next(_ -> 
 				Image.getInfo('tests/assets/sample_resized_$engine.$extension')
-			).next(info -> {
-        assert.is(info.width, 100);
-        assert.is(info.height, 60);
-				return Noise;
-			});
+			);
 	}
 
-	function testFileFormats(done: Void -> Void, engine: Engine, extensions: Array<String>) {
-		return Promise.inParallel(
-			extensions.map(ext -> resize(engine, ext))
-		).handle(res -> {
-      switch res {
-        case Failure(e): throw e;
-        default: done();
-      }
-    });
+	function testFileFormats(engine: Engine, extensions: Array<String>) {
+		for (ext in extensions) {
+			test('$engine $ext', done -> {
+				resize(engine, ext).handle(res -> {
+					switch res {
+						case Success(info):
+							assert.is(info.width, 100);
+							assert.is(info.height, 60);
+							done();
+						case Failure(e):
+							throw e;
+					}
+				});
+			});
+		}
 	}
 
 	#if cli
-  test('image magick', (done) -> {
-    testFileFormats(done, Engine.ImageMagick, ['jpg', 'png', 'bmp', 'gif', 'tiff']);
-  });
-
-  test('image magick', (done) -> {
-    testFileFormats(done, Engine.GraphicsMagick, ['jpg', 'png', 'bmp', 'gif', 'tiff']);
-  });
-
-  test('vips', (done) -> {
-    testFileFormats(done, Engine.Vips, ['jpg', 'png', 'webp', 'tiff']);
-  });
+  testFileFormats(Engine.ImageMagick, ['jpg', 'png', 'bmp', 'gif', 'tiff']);
+	testFileFormats(Engine.GraphicsMagick, ['jpg', 'png', 'bmp', 'gif', 'tiff']);
+  testFileFormats(Engine.Vips, ['jpg', 'png', 'webp', 'tiff']);
 	#end
 
 	#if php
-  test('gd', (done) -> {
-    testFileFormats(done, Engine.GD, ['jpg', 'png', 'bmp', 'gif']);
-  });
+  testFileFormats(Engine.GD, ['jpg', 'png', 'bmp', 'gif']);
 	#end
 });
